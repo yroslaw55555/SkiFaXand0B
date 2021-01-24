2eab39a15b09c321869ed687ab529285c4328659
yroslaw55555 <75557043+yroslaw55555@users.noreply.github.com>
2021-01-24 18:24:44 +0700

    Update XandO.py
    Доработал

--- SkiFaXand0/XandO.py
+++ SkiFaXand0/XandO.py
@@ -28,12 +28,26 @@ def tir_in_L ():
             tir += 1
     return tir
 
+def win():
+    if (L[1][1]==L[2][2]==L[3][3]) or (L[1][3]==L[2][2]==L[3][1]):
+        if L[2][2] != '-':
+            GameWin = str(L[2][2])
+    if (L[1][1]==L[1][2]==L[1][3]) or (L[1][1]==L[2][1]==L[3][1]):
+        if L[1][1] != '-':
+            GameWin = str(L[1][1])
+    if L[1][2]==L[2][2]==L[3][2] or L[2][1]==L[2][2]==L[2][3]:
+        if L[2][2] != '-':
+            GameWin = str(L[2][2])
+    if L[3][1]==L[3][2]==L[3][3] or L[1][3]==L[2][3]==L[3][3]:
+        if L[3][3] != '-':
+            GameWin = str(L[3][3])
+    return GameWin
 
 while tir_in_L ():
     show_game ()
     yx=list(input('Введите через пробел координаты x и у а затем символ:').lower().split())
-    while not(int(yx[0])+1 and int(yx[1])+1):
-        yx=list(input('x и y не могут быть меньше 0:').lower().split())
+    while not(int(yx[0])+1 and int(yx[1])+1) or (((int(yx[0])+1) > 3) or ((int(yx[1])+1) > 3)):
+        yx=list(input('y и x не могут быть меньше 0 или больше 2:').lower().split())
     while L[int(yx[0])+1][int(yx[1])+1] != '-':
         yx=list(input('Эта клетка занята:').split())
     while  yx[2]!='x' and yx[2]!='0':
@@ -42,29 +56,17 @@ while tir_in_L ():
     while past_tur == yx[2]:
         print ('Сейчас не твой ход', past_tur)
         yx=list(input('Введите через пробел координаты x и у а затем символ:').lower().split())
-
     L[int(yx[0])+1][int(yx[1])+1] = yx[2]
-    if (L[1][1]==L[2][2]==L[3][3]) or (L[1][3]==L[2][2]==L[3][1]):
-        if L[2][2] != '-':
-            GameWin = str(L[2][2])
-            break
-    if (L[1][1]==L[1][2]==L[1][3]) or (L[1][1]==L[2][1]==L[3][1]):
-        if L[1][1] != '-':
-            GameWin = str(L[1][1])
-            break
-    if L[1][2]==L[2][2]==L[3][2] or L[2][1]==L[2][2]==L[2][3]:
-        if L[2][2] != '-':
-            GameWin = str(L[2][2])
-            break
-    if L[3][1]==L[3][2]==L[3][3] or L[1][3]==L[2][3]==L[3][3]:
-        if L[3][3] != '-':
-            GameWin = str(L[3][3])
-            break
+
+    win()
+    if GameWin != '':
+        break
     past_tur = str(yx[2])
 
 show_game ()
 
 if GameWin == '':
     GameWin = 'Дружба!!!'
+
 print('Игра закончена выйграл: ', GameWin,'!!!')
 input()
